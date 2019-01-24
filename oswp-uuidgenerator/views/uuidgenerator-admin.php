f79a6a2e1f80fda9240370abbcff34e553190310
BigManzai <39168019+BigManzai@users.noreply.github.com>
2019-01-24 21:42:27 +0100

    Add files via upload

---
+++ oswp-uuidgenerator/views/uuidgenerator-admin.php
@@ -0,0 +1,9 @@
+<!-- This file is used to markup the administration form of the widget. -->
+<?php
+// Hier steht der Code der Funktion(en), die die eigentliche Funktionalität enthält
+
+?>
+
+<!-- Ab hier die Ausgabe im Control-Fenster des Adminbereiches -->
+
+
---
+++ oswp-uuidgenerator/views/uuidgenerator-widget.php
@@ -0,0 +1,14 @@
+<!-- This file is used to markup the public-facing widget. -->
+
+<?php
+// Die Funktionen einbinden
+include "uuid.clas.php";
+
+// Zufalls UUID ausgeben
+
+// UUID aus der Regions.ini.example
+echo "<h2>OpenSim UUID Generator</h2>";
+
+// Neue Regions UUID
+echo "<h5>$v4uuid</h5>";
+?>
