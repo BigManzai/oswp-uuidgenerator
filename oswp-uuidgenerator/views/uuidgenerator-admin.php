e9460868ea285d2b737458955e69f94c365691aa
BigManzai <39168019+BigManzai@users.noreply.github.com>
2019-07-09 12:34:27 +0200

    Delete uuidgenerator-admin.php

--- oswp-uuidgenerator/views/uuidgenerator-admin.php
+++
@@ -1,9 +0,0 @@
-<!-- This file is used to markup the administration form of the widget. -->
-<?php
-// Hier steht der Code der Funktion(en), die die eigentliche Funktionalität enthält
-
-?>
-
-<!-- Ab hier die Ausgabe im Control-Fenster des Adminbereiches -->
-
-
